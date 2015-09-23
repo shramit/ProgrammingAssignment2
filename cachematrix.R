2a902bacb73c2e6b73a486731d702a885187b9a8
shramit <amit.ar.sharma@gmail.com>
2015-09-24 00:35:28 +0800

    Programming Assignment 2
    Programming Assignment 2 to create function which can compute inverse of
    matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,24 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+## Sample run
+##> x <- rbind(c(1,-2),c(-2,1))
+##> invr <- makeCacheMatrix(x)
+###> invr$get()
+#     [,1] [,2]
+#[1,]    1   -2
+#[2,]   -2    1
+#No cache
+
+#> cacheSolve(invr)
+#           [,1]       [,2]
+#[1,] -0.3333333 -0.6666667
+#[2,] -0.6666667 -0.3333333
+
+#From cache
+#> cacheSolve(invr)
+#getting cached data..
+#           [,1]       [,2]
+#[1,] -0.3333333 -0.6666667
+#[2,] -0.6666667 -0.3333333
